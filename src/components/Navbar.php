eb74f57ece36570f9a72bbb69a96655dbfdc1ece
dragos <panzarudragos15@gmail.com>
2026-05-24 21:07:14 +0300

    added download page

--- src/components/Navbar.php
+++ src/components/Navbar.php
@@ -3,7 +3,7 @@
         <h1 id="title-principal">Proiect accidente</h1>
         <div id="header-options">
             <a href="/projectWEB/public/home" class="header-option">Home</a>
-            <a href="" class="header-option">Download Page</a>
+            <a href="/projectWEB/public/download" class="header-option">Download Page</a>
             <a href="/projectWEB/public/map" class="header-option">Map</a>
             <a href="" class="header-option">Upload Page</a>
             <a href="" class="header-option">About</a>
